35c663b2b6e9933c2612423b8355a3156c7d0b5e
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2024-05-07 18:17:11 -0400

    slightly modified model to display discount as percentage

--- NotBK/Models/Promocion.cs
+++ NotBK/Models/Promocion.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NotBK.Models;
 
@@ -9,6 +10,7 @@ public partial class Promocion
 
     public string Nombre { get; set; } = null!;
 
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:P0}")]
     public decimal? Descuento { get; set; }
 
     public virtual ICollection<PromoItem> PromoItems { get; set; } = new List<PromoItem>();
